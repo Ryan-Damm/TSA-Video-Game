3e2f121fbff5eecf92dc3e4a169577fd3131b806
DammLabs <dammryan370@gmail.com>
2025-10-16 15:45:06 -0400

    Commit 10/16/25
    Test commit

--- Assets/PleaseWork.cs
+++ Assets/PleaseWork.cs
@@ -7,4 +7,12 @@ public class PleaseWork : MonoBehaviour
         Debug.Log("Pleae Please PLease PLease plelase PLeae");
         Debug.Log("Annuder test");
     }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+
+    }
 }
